93e0295a6b81f51b954915e7b0972c71f860f48b
Alan Doose <alan.doose@gmail.com>
2020-03-29 11:04:13 -0700

    Added all glfw/glad init functions and renders a blank black screen

--- OpenGLDevelopment/src/main.cpp
+++ OpenGLDevelopment/src/main.cpp
@@ -6,8 +6,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Callback function to be registered to the window that should get called 
+//	each time the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main()
 {
+	///Init stuff
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -24,6 +33,23 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Set callback function to be called each time window is resized
+	///
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
