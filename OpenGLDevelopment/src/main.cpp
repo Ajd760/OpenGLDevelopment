27c884c9c6536b7615c84bb6f4f75f2ba0bf582d
Alan Doose <alan.doose@gmail.com>
2020-03-28 22:03:55 -0700

    Implemented GLFW window

--- OpenGLDevelopment/src/main.cpp
+++ OpenGLDevelopment/src/main.cpp
@@ -8,7 +8,22 @@
 
 int main()
 {
-	std::cout << "Initial commit!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLDevelopment", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create a GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
 
 	return 0;
 }
