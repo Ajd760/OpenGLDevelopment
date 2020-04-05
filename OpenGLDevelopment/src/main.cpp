498cadaa3b9352734bd44245251704d16ffc5844
Alan Doose <alan.doose@gmail.com>
2020-04-04 17:55:40 -0700

    cleaned up old ad-hoc shader code in main

--- OpenGLDevelopment/src/main.cpp
+++ OpenGLDevelopment/src/main.cpp
@@ -20,26 +20,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
 
-/*
-// Ad hoc shaders - temporary
-const char* vertShaderSrc = "#version 330 core\n"
-						"layout (location = 0) in vec3 aPos;\n"
-						"layout (location = 1) in vec3 aColor;\n"
-						"out vec3 ourColor;\n"
-						"void main()\n"
-						"{\n"
-						"	gl_Position = vec4(aPos, 1.0);\n"
-						"	ourColor = aColor;\n"
-						"}\n\0";
-const char* fragShaderSrc = "#version 330 core\n"
-						"out vec4 FragColor;\n"
-						"in vec3 ourColor;\n"
-						"void main()\n"
-						"{\n"
-						"   FragColor = vec4(ourColor, 1.0f);\n"
-						"}\n\0";
-*/
-
 
 int main()
 {
